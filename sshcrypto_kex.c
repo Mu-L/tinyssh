b24aba92a84d700fcb8d6defccae41c5f32c48cd
Jan Mojžíš <jan.mojzis@gmail.com>
2026-09-05 14:48:32 +0000

    sshcrypto: add sntrup761x25519-sha512 KEX

--- sshcrypto_kex.c
+++ sshcrypto_kex.c
@@ -40,6 +40,18 @@ struct sshcrypto_kex sshcrypto_kexs[] = {
         0,
     },
     {
+        "sntrup761x25519-sha512",
+        crypto_kem_sntrup761x25519_enc,
+        crypto_kem_sntrup761x25519_PUBLICKEYBYTES,  /* pk */
+        crypto_kem_sntrup761x25519_CIPHERTEXTBYTES, /* c */
+        crypto_kem_sntrup761x25519_BYTES,           /* k */
+        crypto_hash_sha512,
+        crypto_hash_sha512_BYTES,
+        sntrup761x25519_putkemkey,
+        sshcrypto_TYPEPQCRYPTO,
+        0,
+    },
+    {
         "sntrup761x25519-sha512@openssh.com",
         crypto_kem_sntrup761x25519_enc,
         crypto_kem_sntrup761x25519_PUBLICKEYBYTES,  /* pk */
